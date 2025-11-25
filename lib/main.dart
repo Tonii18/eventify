be66640901bcb34781891c822c4358f28923518b
Jesus <gomez.rojes22@cadiz.salesianos.edu>
2025-11-25 14:06:46 +0100

    Init commit

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
       title: 'Eventify',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
